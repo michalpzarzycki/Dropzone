19766194426b1ff61a5418dc88ab63b7f5bc7335
Michal <michal.p.zarzycki@wp.pl>
2021-04-04 00:26:08 +0200

    Set up dropzone.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const PORT = process.env.PORT || 3000; 
 const app = express();
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log("LISTEN ON PORT ", PORT)
